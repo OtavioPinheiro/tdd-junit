3ff5dcf264a639dc55bfa3abae6563644a22f743
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2025-01-23 21:22:37 -0300

    :white_check_mark: test: Testes de reajuste de 15% e 20% adicionados

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,9 +8,24 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if (desempenho == Desempenho.A_DESEJAR) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-            funcionario.reajustarSalario(reajuste);
+        if (desempenho != null) switch (desempenho) {
+            case A_DESEJAR: {
+                BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
+                funcionario.reajustarSalario(reajuste);
+                    break;
+                }
+            case BOM: {
+                BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
+                funcionario.reajustarSalario(reajuste);
+                    break;
+                }
+            case OTIMO: {
+                BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
+                funcionario.reajustarSalario(reajuste);
+                    break;
+                }
+            default:
+                break;
         }
     }
 
