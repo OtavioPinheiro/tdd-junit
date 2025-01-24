32d90e2e4b1ecd9dff269f7976f3b63a26e42a98
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2025-01-23 21:35:11 -0300

    :recycle: refactor: Cálculo para conceder reajuste refatorada

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,25 +8,9 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if (desempenho != null) switch (desempenho) {
-            case A_DESEJAR: {
-                BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-                funcionario.reajustarSalario(reajuste);
-                    break;
-                }
-            case BOM: {
-                BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-                funcionario.reajustarSalario(reajuste);
-                    break;
-                }
-            case OTIMO: {
-                BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-                funcionario.reajustarSalario(reajuste);
-                    break;
-                }
-            default:
-                break;
-        }
+        BigDecimal percentual = desempenho.percentualReajuste();
+        BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+        funcionario.reajustarSalario(reajuste);
     }
 
 }
